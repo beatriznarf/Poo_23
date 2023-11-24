f683aa9e436ebb45a214b9b1222fa99d49107b33
Beatriz Narciso Fernandes <147112758+beatriznarf@users.noreply.github.com>
2023-11-24 12:10:30 -0300

    Add files via upload

---
+++ Poo-PI/src/main/java/com/mycompany/tela1/Tela1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tela1;
+
+/**
+ *
+ * @author Beatriz
+ */
+public class Tela1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
