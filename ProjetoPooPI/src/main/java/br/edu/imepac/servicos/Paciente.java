acdbc8e22db8b923a31ce3ee9865cc394baf5e9b
Beatriz Narciso Fernandes <147112758+beatriznarf@users.noreply.github.com>
2023-11-24 00:39:32 -0300

    Add files via upload

---
+++ ProjetoPooPI/src/main/java/br/edu/imepac/servicos/Paciente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.imepac.servicos;
+
+/**
+ *
+ * @author 231-001265
+ */
+public class Paciente {
+    
+}
